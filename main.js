99a88dab5abe6131ea676cd13fe2b06b32ded3d4
kshitij-official <kshitij.btech.cs19@iiitranchi.ac.in>
2021-03-08 21:24:56 +0530

    Add To github

---
+++ main.js
@@ -0,0 +1,71 @@
+const $input = document.querySelector("input");
+
+document.querySelectorAll(".num__key").forEach(
+	el => {
+		el.onclick = () => $input.value = $input.value !== "0" ? $input.value + el.innerText : el.innerText;
+	}
+	);
+const buffer = [];
+
+const opCallBack = opName => () =>{
+	let currentVal = parseFloat($input.value);
+
+	if(opName == "percent")
+	{
+		currentVal *= 0.01;
+		$input.value = currentVal;
+	}
+
+	else{
+		if(buffer && buffer.length){
+			buffer.push({value: currentVal});
+
+			const result = evaluate(buffer);
+
+			buffer.push({ value: result});
+			buffer.push({ value: opName});
+
+			$input.value = "";
+		}
+
+		else{
+			buffer.push({ value: currentVal });
+			buffer.push({ value: opName });
+			$input.value = "";
+		}
+	}
+}
+
+const evaluate = buffer => {
+	const secondOperand = buffer.pop().value;
+	const operator = buffer.pop().value;
+	const firstOperand = buffer.pop().value;
+
+	switch(operator){
+		case "add":
+			return firstOperand + secondOperand;
+			break;
+		case "subtract":
+			return firstOperand - secondOperand;
+			break;
+		case "multiply":
+			return firstOperand * secondOperand;
+			break;
+		case "divide":
+			return firstOperand - secondOperand;
+			break;
+		default:
+			return secondOperand;
+	}
+}
+
+for(const opName of ["add", "subtract", "multiply", "divide", "percent"]){
+	document.querySelector(".op__key[op=${opName}]")	
+	.onclick = opCallBack(opName);
+}
+
+
+
+
+
+
